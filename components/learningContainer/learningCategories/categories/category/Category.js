128d4b543ae67c0deb7728f0ebd66bd79822090a
Amer-Alghfili <ameralghfili@gmail.com>
2022-03-06 12:13:08 +0300

    crossOrigin fixed

--- components/learningContainer/learningCategories/categories/category/Category.js
+++ components/learningContainer/learningCategories/categories/category/Category.js
@@ -3,17 +3,16 @@ import Image from "next/image";
 import styles from "./Category.module.css";
 import { categoryContext } from "../../../../../utils/context";
 function loader(src) {
-  console.log("Image src: ", src);
   return `http://localhost:3000/${src}`;
 }
-export default React.forwardRef(({ category }, ref) => {
+
+export default function Category({ category }) {
   const { categoryId, setCategoryId } = useContext(categoryContext);
   const { id, uid, src, alt, title } = category;
   const selectedCategoryId = uid === "all" ? "all" : id;
   return (
     <li
       className={styles.category}
-      ref={ref}
       onClick={() => setCategoryId(selectedCategoryId)}
     >
       <div className={styles.category__imgContainer}>
@@ -29,4 +28,4 @@ export default React.forwardRef(({ category }, ref) => {
       <p className={styles.category__title}>{title}</p>
     </li>
   );
-});
+}
